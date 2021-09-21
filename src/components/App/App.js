c5b7ae68c728807b2eb08bbfcd987080f6ebaa5f
felipe-brando <f.brandotalero@gmail.com>
2021-09-21 16:29:41 +0200

    routes settings

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,7 @@
 // == Import
+import { Route, Switch } from 'react-router-dom';
+import {useSelector} from 'react-redux';
+
 import Connection from '../Connection/index.js';
 import Header from '../Header/index.js';
 import Intro from '../Intro/index.js';
@@ -11,12 +14,34 @@ import './App.scss';
 
 // == Composant
 const App= () => {
+  const logged = useSelector((state) => state.user.logged);
   return (
     <div className="App">
       <Connection />
       <Header />
-      <Intro />
-      <AnnounceList />
+      <Switch>
+        <Route path="/" exact>
+          <Intro />
+          <AnnounceList />
+        </Route>
+        <Route path="/a-propos">
+          <div>A propos</div>
+        </Route>
+        <Route path="/actualites">
+          <div>Actualités</div>
+        </Route>
+        <Route path="/contact">
+          <div>Contact</div>
+        </Route> 
+        <Route path="/espace-perso">
+        {logged ? 
+          <div>Espace personnel</div> :
+          <div>403</div>}
+        </Route> 
+        <Route>  
+          <div>404</div>
+        </Route> 
+      </Switch>
       <Footer />
     </div>
   );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
 import { combineReducers } from 'redux';
 
-
 import userReducer from './user';
+import announceReducer from './classroom';
+import classroomReducer from './announce';
+import lessonReducer from './lesson';
+
 
 const rootReducer = combineReducers({
   user: userReducer,
+  classroom: classroomReducer,
+  announce: announceReducer,
+  lesson: lessonReducer,
 });
 
 export default rootReducer;
