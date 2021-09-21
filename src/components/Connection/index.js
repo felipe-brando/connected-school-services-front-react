cc5becad365d60cee4ebf09d40598de3fc11df3f
felipe-brando <f.brandotalero@gmail.com>
2021-09-21 20:09:20 +0200

    controled fields ans styles

--- src/components/Connection/index.js
+++ src/components/Connection/index.js
@@ -1,20 +1,46 @@
 // == Import
+import { useSelector, useDispatch } from 'react-redux';
+
 import { User } from 'react-feather';
 import { Link } from "react-router-dom";
 
 import './style.scss';
+import LoginForm from './LoginForm';
 // == Composant
-const Connection = () => (
+const Connection = () => {
+  const isOpen = useSelector((state) => state.user.loginOpen);
+  const logged = useSelector((state) => state.user.logged);
+
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch({
+      type: 'TOGGLE_OPEN',
+    });
+  };
+
+  return (
     <div className="connection">
       <ul className="connection__list">
         <li> 
-          <Link to="#" className="connection__item">
+          
+          <Link onClick={handleClick} to="#" className="connection__item">
             <div className="connection__icon"><User /></div>
             <div className="connection__text">Connexion</div>
           </Link>
+          {logged &&
+            <Link to="#" className="connection__item">
+            <div className="connection__icon"><User /></div>
+            <div className="connection__text">Espace perso</div>
+          </Link>
+          }
         </li> 
       </ul>
+      <div className={isOpen ? "login" : "login login--hidden"} >
+  
+      <LoginForm />
+      </div>
     </div>
   );
+}
 
 export default Connection;
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -3,20 +3,39 @@ export const initialState = {
     firstname: 'Jhon',
     lastename: 'Toto',
     image: '',
-    email: 'jhon@toto.com',
+    email: '',
     adress: '10 rue de truc',
     zip_code: '75000',
     city: 'Paris',
     phone: '06000000',
-    password: 'toto',
+    password: '',
     token: '',
     roles: 'élève',
     classroom: 'Troisième A',
     discipline: 'Mathématiques',
+    loginOpen: false,
   };
   
   const reducer = (state = initialState, action = {}) => {
     switch (action.type) {
+      case 'CHANGE_VALUE':
+      return {
+        ...state,
+        [action.key]: action.newValue, 
+      };
+      case 'LOGIN':
+        return {
+          ...state,
+          loginOpen: false,
+          logged: true,
+          email: '',
+          password: '',
+      };
+      case 'TOGGLE_OPEN':
+      return {
+        ...state,
+        loginOpen: !state.loginOpen,
+      };
       default:
         return state;
     }
