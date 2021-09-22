a8dee2a6f546a9f3e26e35ce0065be9c27a366fe
Willy DELERUE <willy.delerue@gmail.com>
2021-09-22 09:59:17 +0200

    form component finished

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,42 +1,64 @@
 import React from 'react';
 
-class Form extends React.Component {}
+import './style.scss';
+
+// class Form extends React.Component {
+
+function Field ({name, value, onChange, children}){
+    return <div className="form-group">
+       <label htmlFor={name}>{children}</label>
+       <input type="text" value={value} onChange={onChange} id={name} name={name} className="form-control"/>
+       </div>
+
+}
+
+class ContactForm extends React.Component {
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            nom:'',
+            prenom:'',
+            newsletter:false
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
 
 
-
-handleChange (event) {
+handleChange = (event) => {
     const name = event.target.name
     const type = event.target.type
-    const value = type === 'checkbox' ? e.target.checked : e.target.value
+    const value = type === 'checkbox' ? event.target.checked : event.target.value
     this.setState({
         [name]: value
     })
 }
 
-handleSubmit (event) {
-    e.preventdefault()
+handleSubmit = (event) => {
+    event.preventdefault()
     const data = JSON.stringify(this.state)
     this.setState({
         nom: '',
         prenom: '',
-        email: ''
+        courriel: ''
     })
 }
 
-form = () => {
-    return 
+// generating a form getting values for the different fields 
+
+render() {
+    return (
       <form className="contact-form">
-         <field name="nom" value={this.state.name} onChange={this.handleChange}>Nom</field>
-         <field name="prenom" value={this.state.surname} onChange={this.handleChange}>Prémom</field>
-         <field name="courriel" value={this.state.courriel} onChange={this.handleChange}>Courriel</field>
+         <Field name="nom" value={this.state.nom} onChange={this.handleChange}>Nom</Field>
+         <Field name="prenom" value={this.state.prenom} onChange={this.handleChange}>Prémom</Field>
+         <Field name="courriel" value={this.state.courriel} onChange={this.handleChange}>Courriel</Field>
          <div className ="form-group">   
          <button class ="btn btn-primary">Envoyer</button>
          </div>
          {JSON.stringify(this.state)}
       </form>
+    )}
 };
 
 
-
-
-export default form;
+export default ContactForm;
