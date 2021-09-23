1098a9a34f70472b6d0b7daa0ea37286b6e6b449
felipe-brando <f.brandotalero@gmail.com>
2021-09-24 00:03:04 +0200

    intro component first styles

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,9 +1,11 @@
 // == Import
-
+import './style.scss';
+import image from '../../assets/img/school.jpg';
 // == Composant
 const Intro = () => (
   <section className="intro">
     <h2 className="intro__title">Nom de l'établissement</h2>
+    <img className="intro__image" src={image} alt="Nom de l'établissement"/>
   </section>
 );
 
