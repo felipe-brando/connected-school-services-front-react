ae0f46357147735ea46d712e8bb2db23854c06ad
felipe-brando <f.brandotalero@gmail.com>
2021-09-26 14:38:47 +0200

    persisting the state to the local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from '../reducers/index.js';
 
-// on importe les middlewares
+import { loadState, saveState, removeState } from './localStorage.js';
+
+// we import the middlewares
 import announceApi from '../middlewares/announceApi';
 import connectionApi from '../middlewares/connectionApi';
 import scheduleApi from '../middlewares/scheduleApi';
-// import APIMiddleware from 'src/middlewares/api';
-// import websocketMiddleware from 'src/middlewares/websocket';
 
-// on met bout à bout tous nos middlewares
+// we call each middleware
 const middlewares = applyMiddleware(
     announceApi,
     connectionApi,
@@ -20,8 +20,27 @@ const middlewares = applyMiddleware(
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancers = composeEnhancers(middlewares);
 
-// on crée le store à qui l'on passe le reducer et les middlewares (avec le devtool)
-//const store = createStore(reducer, enhancers);
-const store = createStore(reducer, enhancers);
+// we need to persisting the state to the local storage
+const persistedState = loadState();
+// we create the store and we put on the store
+// the reducer, the persisted State
+// and the middlewares (with the devtool) 
+const store = createStore(
+    reducer, 
+    persistedState,
+    enhancers
+);
+// when any action is dispached 
+// we save the user state
+// and if the user log off we remove the user state
+store.subscribe(() => {
+    saveState({
+        user: store.getState().user
+    });
+    if (store.getState().user.logged === false) {
+        removeState ({user :store.getState().user})
+    }
+
+});
 
 export default store;
