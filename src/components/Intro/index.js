ba2faa4cb900ba881f477cb91bd2f185f2fdf4b8
felipe-brando <f.brandotalero@gmail.com>
2021-10-06 18:48:21 +0200

    adding styles on about component

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -3,11 +3,11 @@ import './style.scss';
 // == Composant
 const Intro = () => (
   <section className="intro">
-    <h1 className="intro__title">Nom de l'établissement</h1>
+    <h1 className="intro__title">Collège Mark Zuckerberg</h1>
     <div className="intro__content">
       <div className="intro__image" />
     </div>
   </section>
 );
-
+// == Export
 export default Intro;
