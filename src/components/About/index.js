0eb99d76dc5950df6a931506d310fd525e1ae81e
felipe-brando <f.brandotalero@gmail.com>
2021-10-07 09:12:31 +0200

    adding scss vars

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,7 +10,7 @@ const About = () => {
             <h1 className="about__title">Présentation du Collège</h1>           
             <section className="about__content">
                 <p>Bienvenus au Collège Mark Zuckerberg.</p>
-                <p>Notre collège est installé depuis 2014 dans la colline domine la ville. Notre un établissement qui compte environ 500 élèves.</p>
+                <p>Notre collège est installé depuis 2014 dans la colline qui domine la ville. Notre établissement compte environ 500 élèves.</p>
                 <p>L'établissement dispose d'un internat.</p>
                 <p>Toute l’équipe de l’établissement (pédagogie, encadrement et administration) a à cœur la réussite des élèves et leur épanouissement.</p>
             </section>
