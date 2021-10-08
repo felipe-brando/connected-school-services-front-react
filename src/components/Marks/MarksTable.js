e1eac0534cd70c4d66f2d5d8c10223bb19f79326
Willy DELERUE <willy.delerue@gmail.com>
2021-10-08 15:04:09 +0200

    modif

--- src/components/Marks/MarksTable.js
+++ src/components/Marks/MarksTable.js
@@ -1,11 +1,52 @@
 import React from "react";
+<<<<<<< Updated upstream
+=======
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink } from 'react-router-dom';
+
+>>>>>>> Stashed changes
 
 import './style.scss';
 
 const MarksTable = ({ measureDataArray, marksDataArray }) => {
 
     const generalAverage= [];
+<<<<<<< Updated upstream
  
+=======
+    const resources = useSelector((state) => state.lesson.resourcesList);
+    const disciplineId = useSelector((state) => state.user.disciplineId);
+
+    const marksArray = [];
+    const [marksTitle, setMarksTitle] = useState("");
+    
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+     
+        dispatch({
+            type: 'FETCH_DISCIPLINES'
+        });
+    }, [])
+    
+
+    function handleSubmitMarks(e) {
+        e.preventDefault();
+        dispatch({
+            type: 'SEND_MARKS',
+            marksArray: marksArray,
+            marksTitle: marksTitle,
+            disciplineId: disciplineId,
+        })
+        setMarksTitle("");
+    }
+
+    const onMouseOver = () => {
+        ;
+    }
+
+>>>>>>> Stashed changes
     return (
         <table className = "table__content">
             <thead>
@@ -34,7 +75,14 @@ const MarksTable = ({ measureDataArray, marksDataArray }) => {
                               } return true;
                             })  
                         }
+<<<<<<< Updated upstream
                             <td className ="table__content--marks">{(gradeData.join(" | "))}</td>
+=======
+                       
+                        
+                            <td className ="table__content--marks"><button className ="btn__showresources" type = "text" id ="label" value = {marksTitle}  onMouseOver ={resources} label ="test" aria-label ="matière à afficher">{(gradeData.join(" | "))}</button> </td>
+                            
+>>>>>>> Stashed changes
                             
                             <td className = "table__content--average">{parseInt(gradeData.reduce((a,b) => a+b, 0))/gradeData.length}</td> 
                         </tr>);}   
